a8734cf0fa0901552e7286cab0862457951dda2d
christiandoramo <christiandoramo@gmail.com>
2023-02-02 00:33:29 -0300

    imersao dev dia 3 concluido

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
 // foi necessario a instalação do ejs, express;
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const conversor = require("./dia 1 - conversor de moedas/conversor"); //rodando cotação
-const mentalista = require("./dia 2 - mentalista/mentalista"); //rodando cotação
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const conversor = require('./dia 1 - conversor de moedas/conversor')
+const mentalista = require('./dia 2 - mentalista/mentalista')
+const aluraflix = require('./dia 3 - aluraFlix/aluraflix')
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false })); // decodificação com o bodyparser
-app.use(bodyParser.json()); // lê agora os json
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false })) // decodificação com o bodyparser
+app.use(bodyParser.json()) // lê agora os json
 
-app.get("/", (requisicao, resposta) => {
-  resposta.render("index");
-});
+app.get('/', (requisicao, resposta) => {
+  resposta.render('index')
+})
 
 // DIA 1 - CONVERSOR
-app.get("/converter", (requisicao, resposta) => {
-  const valorEmReal = requisicao.query.valorEmReal;
-  const moeda = requisicao.query.moeda;
-  const pesquisa = conversor.pesquisar(moeda, valorEmReal);
-  resposta.render("dia_1", {
+app.get('/converter', (requisicao, resposta) => {
+  const valorEmReal = requisicao.query.valorEmReal
+  const moeda = requisicao.query.moeda
+  const pesquisa = conversor.pesquisar(moeda, valorEmReal)
+  resposta.render('dia_1', {
     pesquisa: pesquisa,
-  });
-});
+  })
+})
 
 // DIA 2 - MENTALISTA
-app.get("/advinhar", (requisicao, resposta) => {
-  const valorDigitado = requisicao.query.valorDigitado;
-  const maiorOuMenor = mentalista.advinhar(valorDigitado);
-  resposta.render("dia_2", {
+app.get('/advinhar', (requisicao, resposta) => {
+  const valorDigitado = requisicao.query.valorDigitado
+  const maiorOuMenor = mentalista.advinhar(valorDigitado)
+  resposta.render('dia_2', {
     valorDigitado: valorDigitado,
     valorPrevio: mentalista.valorPrevio,
     maiorOuMenor: maiorOuMenor,
-  });
-});
+  })
+})
 
-app.listen(2023, () => console.log("Conexão efetuada com sucesso!!!"));
+// DIA 3 - ALURAFLIX
+app.get('/aluraflix', (req, res) => {
+  res.render('dia_3', {
+    filmes: aluraflix.filmes,
+  })
+})
+
+app.listen(2023, () => console.log('Conexão efetuada com sucesso!!!'))
